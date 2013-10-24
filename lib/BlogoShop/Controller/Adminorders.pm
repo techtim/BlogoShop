4749f2c3e7f37f7fea137ba7eceb675ac670232b
Tim Tavlintsev <timmy88@gmail.com>
2013-10-24 23:10:32 +0400

    30orders

--- lib/BlogoShop/Controller/Adminorders.pm
+++ lib/BlogoShop/Controller/Adminorders.pm
@@ -8,7 +8,7 @@ use utf8;
 
 use constant ORDER_FILTERS => qw (status);
 use constant ORDER_STATUS => qw (new proceed assembled finished canceled changed wait_delivery wait_courier self_delivery sent_courier sent_post sent_ems);
-use constant ORDERS_ON_PAGE => 10;
+use constant ORDERS_ON_PAGE => 30;
 my $status_regex = join '|', ORDER_STATUS;
 
 sub list {
@@ -26,34 +26,41 @@ sub list {
 		}}
 	);
 
-	my $orders_count = $counter->{count};
+	my $orders_count = $counter	->{count};
 
 	$counter = {map {$_->{status} => $_->{count} } @{$counter->{retval}}};
 
 	# Paging
-    my $skip = ORDERS_ON_PAGE *
-       ($self->req->param('page') && $self->req->param('page') =~ /(\d+)/ ? ($1>0 ? $1-1 : 0) : 0);
+	my $skip = ORDERS_ON_PAGE *
+	   ($self->req->param('page') && $self->req->param('page') =~ /(\d+)/ ? ($1>0 ? $1-1 : 0) : 0);
 
-    my $pager_url  =  $self->req->url->path->to_string.'?'.$self->req->url->query->to_string;
-    $pager_url =~ s!csrftoken=[^\&]+\&?!!;
-    $pager_url =~ s!\&?page=\d+\&?!!;
-    $pager_url .= $pager_url =~ m!\?$! ? '' : '&';
+	my $pager_url  =  $self->req->url->path->to_string.'?'.$self->req->url->query->to_string;
+	$pager_url =~ s!csrftoken=[^\&]+\&?!!;
+	$pager_url =~ s!\&?page=\d+\&?!!;
+	$pager_url .= $pager_url =~ m!\?$! ? '' : '&';
 
 	$self->stash($_) ? $filter->{$_} = $self->stash($_) : () foreach ORDER_FILTERS;
-	my $orders = [
+
+	# FETCH ALL ORDERS WITH FILTER TO COUNT SUM
+	my $orders = [$self->app->db->orders->find($filter)->sort({_id => -1})->all];
+	my $orders_sum = 0;
+	foreach my $order (@$orders) {
+		foreach (@{$order->{items}}) {
+			$orders_sum += $_->{price}*$_->{count};
+		}
+	}
+
+	# FETCH ORDERS FOR PAGING
+	$orders = [
 		$self->app->db->orders->find($filter)->sort({_id => -1})->
 			skip($skip)->limit(ORDERS_ON_PAGE)->all
 	];
-	my $item   = BlogoShop::Item->new($self);
-
-	$filter->{orders_sum} = 0;
 	foreach my $order (@$orders) {
 		foreach (@{$order->{items}}) {
 			$_->{info} = $item->get($_->{_id}, $_->{sub_id});
 			$order->{sum} += $_->{price}*$_->{count};
 		}
 		$order->{order_id} 	= ($order->{_id}->{value}=~/^(.{8})/)[0];
-		$filter->{orders_sum} += $order->{sum};
 	}
 
 	$self->stash('error_message' => $self->flash('error_message')) if $self->flash('error_message');
@@ -61,16 +68,17 @@ sub list {
 
 	return $self->render( 
 		%$filter,
-        orders => $orders,
-        orders_count => $orders_count,
-        counter => $counter,
-        cur_page  => $self->req->param('page') || 1,
-        pages => int( 0.99 + ( $filter->{status} ? $counter->{$filter->{status}} : $orders_count ) / ORDERS_ON_PAGE ),
-        pager_url  => $pager_url,
-        host => $self->req->url->base,
-        template => 'admin/orders',
-        format => 'html',
-    );
+		orders => $orders,
+		orders_count => $orders_count,
+		orders_sum => $orders_sum,
+		counter => $counter,
+		cur_page  => $self->req->param('page') || 1,
+		pages => int( 0.99 + ( $filter->{status} ? $counter->{$filter->{status}} : $orders_count ) / ORDERS_ON_PAGE ),
+		pager_url  => $pager_url,
+		host => $self->req->url->base,
+		template => 'admin/orders',
+		format => 'html',
+	);
 }
 
 sub update {
@@ -82,13 +90,30 @@ sub update {
 		$self->app->db->orders->remove({_id => MongoDB::OID->new(value => $self->stash('id'))}) 
 			if $_ && $_ eq 'delete' && $self->stash('admin')->{login} eq $self->config('order_delete_power');
 	}
-	$self->app->db->orders->update(
-		{_id => MongoDB::OID->new(value => $self->stash('id'))}, 
-			{ '$push' => { comments => 
-				{ login => $self->stash('admin')->{login}, title => $self->req->param('comment.title'), text => $self->req->param('comment.text')} 
-			} }
-	) if $self->req->param('comment.title') || $self->req->param('comment.text');
-
+	if ($self->req->param('comment.title') || $self->req->param('comment.text') ) {
+		$self->app->db->orders->update(
+			{_id => MongoDB::OID->new(value => $self->stash('id'))}, 
+				{ '$push' => { comments => 
+					{ login => $self->stash('admin')->{login}, title => $self->req->param('comment.title'), text => $self->req->param('comment.text')} 
+				} }
+		);
+		my $mail = $self->mail(
+			to      => $self->config('superadmin_mail'),
+			# cc		=> $self->config('superadmin_mail'),
+			from    => 'noreply@'.$self->config('domain_name'),
+			subject => 'новый комментарий в заказе №'.($self->stash('id')=~/^(.{8})/)[0],
+			type 	=> 'text/html',
+			format => 'mail',
+			data => $self->render_mail(
+				template => 'order_mail', 
+				admin => $self->stash('admin'),
+				order_id => ($self->stash('id')=~/^(.{8})/)[0], 
+				title => $self->req->param('comment.title'),
+				text => $self->req->param('comment.text')
+			),
+			handler => 'mail',
+		);
+	}
 	$self->call_courier() if $self->req->param('courier');
 
 	return $self->redirect_to('/admin/orders/'.$self->stash('status'));
